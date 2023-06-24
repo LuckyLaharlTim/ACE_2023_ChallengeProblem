d1cbf177a8f35bcb1be52226ccc2e2fabe2818c8
pattydubbs <63208948+pattydubbs@users.noreply.github.com>
2023-06-24 19:44:49 -0400

    week 1 final neural net

--- 3-MachineLearning/neaural_net.py
+++ 3-MachineLearning/neaural_net.py
@@ -2,9 +2,11 @@ import re
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import csv
+from functools import partial
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 import random
 
 feature_df = pd.read_csv('features.csv')
@@ -32,20 +34,24 @@ print(y[0])
 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(1297, 1)),
-  tf.keras.layers.Dense(3000, activation='sigmoid'),
-  tf.keras.layers.Dense(3000, activation='sigmoid'),
-  tf.keras.layers.Dense(1024, activation='sigmoid'),
-  tf.keras.layers.Dense(512, activation='sigmoid'),
-  tf.keras.layers.Dense(128, activation='sigmoid'),
-  tf.keras.layers.Dense(10)
+  tf.keras.layers.Dense(2800, activation=partial(tf.nn.leaky_relu, alpha=.01)),
+  tf.keras.layers.Dense(4096, activation=partial(tf.nn.leaky_relu, alpha=.01)),
+  tf.keras.layers.Dense(2048, activation=partial(tf.nn.leaky_relu, alpha=.01)),
+  tf.keras.layers.Dropout(.125),
+  tf.keras.layers.Dense(1024, activation=partial(tf.nn.leaky_relu, alpha=.01)),
+  tf.keras.layers.Dense(512, activation=partial(tf.nn.leaky_relu, alpha=.01)),
+  tf.keras.layers.Dense(128, activation=partial(tf.nn.leaky_relu, alpha=.01)),
+  tf.keras.layers.Dense(2, activation='sigmoid')
 ])
 
+
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(0.001),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    optimizer=tf.keras.optimizers.Adam(0.0008),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
 )
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
 
-model.fit(x = X_train, y = y_train, epochs=6, validation_data = (X_test, y_test))
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+
+model.fit(x = X_train, y = y_train, epochs=11, validation_data = (X_test, y_test))
